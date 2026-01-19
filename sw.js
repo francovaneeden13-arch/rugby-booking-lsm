fdba4b732ebbcdafcfdbb59261b84416fe4de274
francovaneeden13-arch <francovaneeden13@gmail.com>
2026-01-19 16:49:44 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAdL6uRoDFjETPISecWgM83JNPwnJk-lb8",
+    authDomain: "rugby-equipment-booking.firebaseapp.com",
+    projectId: "rugby-equipment-booking",
+    storageBucket: "rugby-equipment-booking.firebasestorage.app",
+    messagingSenderId: "248587029060",
+    appId: "1:248587029060:web:63fcd7199beaea82b1ca11",
+    measurementId: "G-T86Y3LB50Z"
+};
